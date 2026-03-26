26607bc2a0a500968a688e2447d902fec7b10599
iluvsteez <thandoseitshiro42@gmail.com>
2026-03-26 07:15:53 +0200

    Final sub

--- freedom_fighter.js
+++ freedom_fighter.js
@@ -1,46 +1,53 @@
-let images = document.querySelectorAll(".pics img"); //selects all images in main container
-let favourites = document.getElementById("favourites");
+let images = document.querySelectorAll("#pics img");
+let favourites = document.getElementById("favorites");
 let actions = document.getElementById("actions");
-let counter = document.getElementById("counter");
 
 let count = 0;
-//used loop so each pic responds
+let total = images.length;
+
+// create counter display
+let counter = document.createElement("p");
+counter.textContent = "Remaining: " + total;
+document.body.insertBefore(counter, actions);
+
+// move to favourites
 for (let i = 0; i < images.length; i++) {
 
     images[i].addEventListener("click", function () {
-//prevent duplicate clicks
+
         if (images[i].classList.contains("selected")) {
             return;
         }
 
-        favourites.appendChild(images[i]);//move img to favs
+        favourites.appendChild(images[i]);
 
         images[i].classList.add("selected");
-        images[i].style.border = "3px solid green";//styling in js so user can see choice
+        images[i].style.border = "3px solid green";
 
         count++;
 
         let li = document.createElement("li");
-        li.textContent = "Moved " + images[i].src + " to favourites";
-        actions.appendChild(li); //records user & item added when img is moved
+        li.textContent = "Moved " + images[i].src + " to favorites";
+        actions.appendChild(li);
 
-        alert("Image " + (i + 1) + " selected as favourite number " + count);//order of img selection
+        alert("Image " + (i + 1) + " selected as favorite number " + count);
 
-        counter.textContent = "Remaining: " + (images.length - count);//how many images are left to choose
+        counter.textContent = "Remaining: " + (total - count);
 
-        if (count === images.length) {
+        if (count === total) {
             alert("All images have been selected!");
         }
     });
 }
-//goes back to normal
+
+// revert back
 favourites.addEventListener("click", function (event) {
 
     let img = event.target;
 
-    if (img.tagName !== "IMG") return;//makes sure only img triggers action
+    if (img.tagName !== "IMG") return;
 
-    document.querySelector(".pics").appendChild(img);
+    document.getElementById("pics").appendChild(img);
 
     img.classList.remove("selected");
     img.style.border = "";
@@ -51,5 +58,8 @@ favourites.addEventListener("click", function (event) {
     li.textContent = "Reverted " + img.src + " back to main list";
     actions.appendChild(li);
 
-    counter.textContent = "Remaining: " + (images.length - count);
+    counter.textContent = "Remaining: " + (total - count);
 });
+
+
+
